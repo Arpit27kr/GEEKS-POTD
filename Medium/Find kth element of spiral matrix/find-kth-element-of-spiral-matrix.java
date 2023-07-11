75c6883497463b8f7bbea1d06ca44c6fb30220b4
Arpit <arpit27kr@gmail.com>
2023-07-11 08:45:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find kth element of spiral matrix/find-kth-element-of-spiral-matrix.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Find_Given_Element_Of_Spiral_Matrix 
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t > 0)
+		{
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int k = sc.nextInt();
+			int arr[][] = new int[1000][1000];
+			for(int i=0; i<n; i++)
+			{
+				for(int j=0; j<m; j++ )
+				{
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			System.out.println(new Solution().findK(arr, n, m, k));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int findK(int[][] A, int n, int m, int k) {
+        int[] result = new int[n * m];
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = m - 1;
+        int direction = 0;
+        int index = 0;
+
+        while (top <= bottom && left <= right) {
+            if (direction == 0) {
+                for (int i = left; i <= right; i++) {
+                    result[index++] = A[top][i];
+                }
+                top++;
+            } else if (direction == 1) {
+                for (int i = top; i <= bottom; i++) {
+                    result[index++] = A[i][right];
+                }
+                right--;
+            } else if (direction == 2) {
+                for (int i = right; i >= left; i--) {
+                    result[index++] = A[bottom][i];
+                }
+                bottom--;
+            } else if (direction == 3) {
+                for (int i = bottom; i >= top; i--) {
+                    result[index++] = A[i][left];
+                }
+                left++;
+            }
+
+            direction = (direction + 1) % 4;
+        }
+
+        return result[k - 1];
+    }
+}
